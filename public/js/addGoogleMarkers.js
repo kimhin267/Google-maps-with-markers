7d7d9ac508acbf142fb7b1b831d783b0ffed87d0
Kim Hin <kim.hin267@gmail.com>
2014-08-22 14:44:34 -0400

    Added google maps height and info window width. A patch for adding markers, zoom, and appending results

--- public/js/addGoogleMarkers.js
+++ public/js/addGoogleMarkers.js
@@ -1,22 +1,66 @@
+var markers = [];
+
 // List of locations in with {coordinates: [lat,lng]}
 function listOfLocations(places) {
 	deleteOverlays();
     for (var i = 0; i < places.length; i++) {
     	addMarker(places[i]);
   	}
+  	setBounds();
 }
 
-// Adds marker(s) to the map
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
+}
+
+// Removes the overlays from the map, but keeps them in the array.
+function clearOverlays() {
+	setAllMap(null);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteOverlays() {
+	clearOverlays();
+	markers = [];
+}
+
+// Adds a marker to the map
 function addMarker(place) {
-	var coordinates = place.coordinates;
-	var lat = coordinates[0];
-	var lng = coordinates[1];
-	markers.push(new google.maps.Marker({
+	var coordinates = place.geometry.location;
+	var keys = Object.keys(coordinates);
+	var lat = coordinates[keys[0]];
+	var lng = coordinates[keys[1]];
+
+	var contentString = '<div class="infowindow"><div>'+place.name+'</div><div>'+place.vicinity+'</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+	    content: contentString,
+	    maxWidth: 300
+	});
+
+	var marker = new google.maps.Marker({
 		position: new google.maps.LatLng(lat, lng),
 		map: map,
 		draggable: false,
-		title: place.company_name +"\n"+place.full_address,
+		title: place.name,
 		animation: google.maps.Animation.DROP
-	}));
-	
+	})
+
+	markers.push(marker);
+
+	google.maps.event.addListener(marker, 'click', function() {
+    	infowindow.open(map,marker);
+  	});
+}
+
+function setBounds() {
+
+    var bounds = new google.maps.LatLngBounds();
+    for (var i=0; i < markers.length; i++) {
+        bounds.extend(markers[i].getPosition());
+    }
+    map.fitBounds(bounds);
 }
--- public/js/getPlaces.js
+++ public/js/getPlaces.js
@@ -1,19 +1,40 @@
+// Model
+searchResults = {
+	results: []
+}
+
+// variables
+current_animated_marker = null;
+
 //Queries user's input and displays results from the query.
-$("#queryAdress").click(function() {
-	var address = $("#adress").val();
-	var companyName = $("#companyName").val();
-	getLocation(companyName, address);
+$(document).ready(function(){
+	$("#queryAddress").click(function() {
+		var address = $("#address").val();
+		var companyName = $("#companyName").val();
+		codeAddress(companyName, address);
+	});
 });
 
+function appendResults(action) {
+	$('#geocode_results').html("");
+	var toDrop = [];
+
+	for (var i = 0; i < searchResults.results.length; i ++) {
+		$('#geocode_results').append('<div class="content"><input class="hide" name="geocode_result" type="radio" value="'+searchResults.results[i].name+', '+searchResults.results[i].vicinity+'"/>'+searchResults.results[i].name+', '+searchResults.results[i].vicinity+'</div><input type="hidden" value="'+searchResults.results[i].geometry.location+'"/></div>');
+		toDrop.push(searchResults.results[i]);
+	}
+	listOfLocations(toDrop);
+}
+
 function codeAddress(companyName, address) {
+	companyName = "";
 	//Queries user's input and displays results from the query.
 	geocoder.geocode( { 'address': address }, function(results, status) {
 		map_center = results[0].geometry.location;
 
 		var request = {
 			location: map_center,
-			radius: 1600,
-			types: ['food'],
+			radius: 1600
 		}
 
 		if (companyName.length != 0){
@@ -26,14 +47,12 @@ function codeAddress(companyName, address) {
 	  	searchResults.results = [];
 
 	  	_.observe(searchResults.results, function() {
-			// console.log("Filtered results changed");
 			appendResults("none");
 		});
 
 	  	function callback(results, status) {
 	  		if (status == google.maps.places.PlacesServiceStatus.OK) {
 	  			map.setCenter(map_center);
-				map.setZoom(13);
 				$("#geocode_results").show();
 
 		  		results.forEach(function(result) {
@@ -41,7 +60,6 @@ function codeAddress(companyName, address) {
 		  		});
 		  	}
 		  	else{
-				// alert("Geocode was not successful for the following reason: " + status);
 				$("#geocode_results").hide();
 				$(".errorMessage").append("<p>No results found, Please try again.</p>");
 		  	}
